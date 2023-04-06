1266fe8f08a1c4641d0060a2c5dcecdccb716373
Nelsonfrank <nelsonfr78@outlook.com>
2023-04-06 23:51:25 +0300

    feat: Add blog post views

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from .models import Post
 
 # Create your views here.
+
+def post_list(request):
+    posts = Post.objects.filter(status=1).order_by('-created_on')
+    context = {'posts': posts}
+    return render(request, 'index.html', context)
+
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    context = {'post': post}
+    return render(request, 'post_detail.html', context)
